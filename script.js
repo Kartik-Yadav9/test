c3c732b565f090c6d0c35c73e470e17d5fb72b9a
Kartik Yadav <kartikyadav0999@gmail.com>
2024-10-05 15:51:13 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+let form= document.getElementById('registrationForm')
+
+    form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    const formData = {
+      first_name: document.getElementById('first_name').value,
+      last_name: document.getElementById('last_name').value,
+      email: document.getElementById('email').value,
+      phone_number: document.getElementById('phone_number').value,
+      password: document.getElementById('password').value
+    };
+
+   
+    console.log(formData);
+
+    form.reset();
+  });
